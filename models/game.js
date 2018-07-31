0eeeda3580491ae0bc454f921411c9c074f0069c
Adams1991 <adams121@hotmail.co.uk>
2018-07-31 19:37:18 +0100

    refactor game to attempt extensions

--- models/game.js
+++ models/game.js
@@ -1,31 +1,28 @@
-const Game = function(player_1, player_2, deck){
-  this.player_1 = player_1;
-  this.player_2 = player_2;
-  this.deck = deck;
+const Card = require('./card.js');
+const Player = require('./player.js');
+
+const Game = function(){
+  this.players = []
+  this.deck = []
 }
 
 Game.prototype.addCard = function (card) {
   this.deck.push(card);
 };
 
-Game.prototype.deal = function () {
-  this.player_1.hand.push(this.deck.pop());
-  this.player_2.hand.push(this.deck.pop());
+Game.prototype.addPlayer = function (player) {
+  this.players.push(player);
 };
 
-Game.prototype.play = function (category, playerChoosing) {
-let player1FirstCard = this.player_1.hand[0];
-let player2FirstCard = this.player_2.hand[0];
+Game.prototype.deal = function () {
+
+};
+
+Game.prototype.play = function (category) {
+
+};
 
 
-
-if (player1FirstCard[category] >= player2FirstCard[category]){
-  this.player_1.hand.push(player2FirstCard);
-  this.player_2.hand.shift();
-}else{
-  this.player_2.hand.push(player2FirstCard);
-  this.player_1.hand.shift();
-}
 
 };
 
