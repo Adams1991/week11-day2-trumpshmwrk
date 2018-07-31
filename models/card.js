0dd6ee6ab35bbdd427f39c34dc0701287d1e4abd
Adams1991 <adams121@hotmail.co.uk>
2018-07-31 15:58:06 +0100

    card tests passed

---
+++ models/card.js
@@ -0,0 +1,8 @@
+const Card = function(name, strength, intelligence, agility){
+  this.name = name;
+  this.strength = strength;
+  this.intelligence = intelligence;
+  this.agility = agility;
+}
+
+module.exports = Card;
--- models/specs/card_spec.js
+++ models/specs/card_spec.js
@@ -14,19 +14,19 @@ describe("Card", function() {
     assert.deepStrictEqual(result, "Superman")
   });
 
-  xit("should have strength", function(){
+  it("should have strength", function(){
     const result = card.strength;
     assert.deepStrictEqual(result, 4)
 
   });
 
-  xit("should have intelligence", function(){
+  it("should have intelligence", function(){
     const result = card.intelligence;
     assert.deepStrictEqual(result, 5)
 
   });
-  
-  xit("should have agility", function(){
+
+  it("should have agility", function(){
     const result = card.agility;
     assert.deepStrictEqual(result, 6)
 
